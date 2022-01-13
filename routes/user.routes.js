67c905bd7aefcf1194ed12a5da280b5451288491
mrtchou2 <91872954+mrtchou2@users.noreply.github.com>
2022-01-13 21:19:01 +0100

    Add files via upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 
-
+/*
+moscou-2021 (github.openclassrooms)
+*/
 router.post("/register", authController.signUp);
 
 module.exports = router;
